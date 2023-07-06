aa8909c97840457ff4e92a9bd89918e3ebb6bb50
aaronsmithtv <86425954+aaronsmithtv@users.noreply.github.com>
2023-07-06 13:53:29 +0100

    Comment out hipfile reload callback

--- hda_py/OnCreated.py
+++ hda_py/OnCreated.py
@@ -9,12 +9,10 @@ node.setColor(blue)
 # Load initially pathed file
 node.parm("hp_file_reload").pressButton()
 
+# # register python callback to be called on hip file event
+# # e.g loading, saving: for reloading hpaint cache
+# def hpaint_reload_se_callback(event_type):
+#     node.parm("hp_file_reload").pressButton()
 
-# register python callback to be called on hip file event
-# e.g loading, saving: for reloading hpaint cache
-def hpaint_reload_se_callback(event_type):
-    node.parm("hp_file_reload").pressButton()
-
-
-with hou.undos.disabler():
-    hou.hipFile.addEventCallback(hpaint_reload_se_callback)
+# with hou.undos.disabler():
+#     hou.hipFile.addEventCallback(hpaint_reload_se_callback)
